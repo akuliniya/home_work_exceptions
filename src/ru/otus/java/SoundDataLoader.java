2bdc0cb11933ae75f205c39af278ef994a18cd9b
Yuliya Yuzeeva <akuliniya@mail.ru>
2021-11-22 17:54:15 +0300

    v2

--- src/ru/otus/java/SoundDataLoader.java
+++ src/ru/otus/java/SoundDataLoader.java
@@ -1,12 +1,17 @@
 package ru.otus.java;
 
+import java.net.SocketException;
+
 public class SoundDataLoader {
     Server server;
 
     public void loadData(Server server) {
         try (ConnectionEmulator connection = server.getConnection()){
-            throw new Exception();
-        } catch (Exception e) {
+            throw new SocketException();
+        } catch (SocketException e) {
+            e.printStackTrace();
+            System.err.println();
+        }catch (Exception e){
             e.printStackTrace();
         }
     }
